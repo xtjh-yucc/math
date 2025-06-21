e85640472a36ead2eee494757264ebcdd1e4e5e7
xtjh-yucc <164116013+xtjh-yucc@users.noreply.github.com>
2025-06-21 13:33:25 +0800

    Add files via upload

---
+++ 鑰匙製造所/service-worker.js
@@ -0,0 +1,39 @@
+
+const CACHE_NAME = 'key-factory-cache-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon.png',
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching app shell');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(response => {
+      return response || fetch(evt.request);
+    })
+  );
+});
